52bca9adde1ae06bf76a89a7c35aa2a49d97bd7c
robertocannella <robertocannella@gmail.com>
2023-12-17 21:15:14 -0500

    Add TCP Client

--- WebServer/CGI-Executables/api.sh
+++ WebServer/CGI-Executables/api.sh
@@ -25,7 +25,39 @@ version=$(echo "$HTTP_X_API_VERSION" | tr '[:lower:]' '[:upper:]')
 # MAKE A CALL TO TCP SERVER
 
 
+for ATTEMPT in $($SEQ_CMD "$MAX_ATTEMPTS"); do
+		log_debug "tcpclient request attempt:$ATTEMPT" ;
+		status_capture=0;
+#     setuidgid www-data /usr/bin/tcpserver -RH -l0 127.0.0.1 9999 sh -c /etc/service/howdyd/howdyd.sh
+#		  $TCP_CLIENT -RHl0 -T 5 -- "$SERVER_IP" "$SERVER_PORT" $FWKWRAPPER ${PASSWORKDIR};
+		$TCP_CLIENT -RH -l0  -- "$SERVER_IP" "$SERVER_PORT" sh -c /etc/service/howdyd/howdyd.sh;
+		status_capture=${PIPESTATUS[0]}
+		log_debug "tcpclient exit status $status_capture"
+#		if [ "$statuscap" -eq "0" ] || [ "$statuscap" -eq "124" ] || [ "$statuscap" -eq "141" ]; then
+#                        if [ ! -s "${WORKDIR}/output" ]; then
+#				log_debug "tcpclient done but output is empty" ;
+#				err_stdout "There was a problem processing your request. System administrators have been notified and will address the issue. "
+#			exit 1
+#			fi
 
+    if [ "$status_capture" -eq 111 ]; then
+        log_error "Error: Unable to connect to $SERVER_IP on port $SERVER_PORT"
+    fi
+
+	  # end of output test
+			log_debug "tcpclient request finished" ;
+#			DATECMD > ${WORKDIR}/finished
+      exit 0
+#			clean_up 0
+#		else
+#		# bad exit value statuscap
+#			export DEBUG=2
+#			touch ${WORKDIR}/error
+#			log_debug "tcpclient failed waiting for $[ATTEMPT*2] "
+#			sleep $((ATTEMPT*2))
+#			ATTEMPT=$((ATTEMPT+1))
+#		fi # end of statuscap test
+	done
 
 # END MAKE A CALL TO TCP SERVER
 
--- WebServer/Project/log/debug.sh
+++ WebServer/Project/log/debug.sh
@@ -2,6 +2,17 @@
 
 source ../../env.sh
 
+
+log_info() {
+    if [ "$APP_INFO_MODE" != "true" ]; then
+        # Debug mode is not true, so return early and don't log
+        return
+    fi
+
+    # Proceed with logging
+    echo "$(date): $*" >> "$INFO_LOG"
+}
+
 log_debug() {
     if [ "$APP_DEBUG_MODE" != "true" ]; then
         # Debug mode is not true, so return early and don't log
@@ -11,4 +22,12 @@ log_debug() {
     # Proceed with logging
     echo "$(date): $*" >> "$DEBUG_LOG"
 }
+log_error() {
+    if [ "$APP_ERROR_MODE" != "true" ]; then
+        # Debug mode is not true, so return early and don't log
+        return
+    fi
 
+    # Proceed with logging
+    echo "$(date): $*" >> "$ERROR_LOG"
+}
